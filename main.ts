61392a0541f1b65db851eed3865b79f0eb7dac4e
Wim Dejonghe <wim.dejonghe@vives.be>
2021-08-19 16:22:08 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,17 @@
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    for (let index = 0; index < 5; index++) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(500)
+    }
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
